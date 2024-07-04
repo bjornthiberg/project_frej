3206b967d1985b0638ce7dd42b875ed5fd234cd5
Björn Thiberg <bthiberg@kth.se>
2024-07-04 13:21:18 +0200

    feat: add error message when API is inaccessible (#32)

--- client/src/components/DataRetrieval.jsx
+++ client/src/components/DataRetrieval.jsx
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import CircularProgress from '@mui/material/CircularProgress';
 import LineChart from './LineChart';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const DataRetrieval = ({ endpoint, selectedOption, pageSize }) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            setLoading(true);
             try {
+                setLoading(true);
+                setError(null);
                 const response = await axios.get(endpoint);
-                const sensorData = response.data.data.slice(-pageSize);
+                const sensorData = response.data.data.slice(-pageSize); // Get the latest {pageSize} number of data points
                 setData(sensorData);
             } catch (error) {
                 console.error('Error fetching data:', error);
+                setError('Failed to fetch data. Please try again later.');
             } finally {
                 setLoading(false);
             }
@@ -24,7 +29,25 @@ const DataRetrieval = ({ endpoint, selectedOption, pageSize }) => {
         fetchData();
     }, [endpoint, selectedOption, pageSize]);
 
-    return loading ? <CircularProgress /> : <LineChart data={data} selectedOption={selectedOption} />;
+    if (loading) {
+        return (
+            <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+                <CircularProgress />
+            </Box>
+        );
+    }
+
+    if (error) {
+        return (
+            <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+                <Typography color="error" variant="h6">
+                    {error}
+                </Typography>
+            </Box>
+        );
+    }
+
+    return <LineChart data={data} selectedOption={selectedOption} />;
 };
 
 export default DataRetrieval;
