28a3dc7b8bddb7c4366c51927b07d51b9e1eaba7
Björn Thiberg <bjorn.thiberg@gmail.com>
2024-07-11 13:07:13 +0200

    feat: interface with new hat, remove old sensors (#83)

--- rpi_scripts/environment.py
+++ rpi_scripts/environment.py
@@ -1,74 +1,55 @@
 import time
-from rpi_scripts.sensors import BME280  # Atmospheric Pressure/Temperature and humidity
-from rpi_scripts.sensors import LTR390  # UV
-from rpi_scripts.sensors import TSL2591  # Light
-from rpi_scripts.sensors import SGP40  # Air Quality
 import datetime
 import logging
+from sense_hat import SenseHat
 
 logger = logging.getLogger(__name__)
 
 
 class Environment:
     def __init__(self):
-        self.atmosphere = BME280()
-        self.atmosphere.get_calib_param()
-        self.light = TSL2591()
-        self.uv = LTR390()
-        self.air_quality = SGP40()
+        self.sense = SenseHat()
 
     def get_sensor_data(self):
         reading_time = datetime.datetime.now().isoformat()
+
         try:
-            bme_data = self.atmosphere.readData()
-            pressure = bme_data[0]
-            temperature = bme_data[1]
-            humidity = bme_data[2]
+            temperature = self.sense.get_temperature()
         except Exception as e:
-            logging.error(f"Error reading BME280 data: {e}")
-            pressure = None
+            logging.error(f"Error reading temperature: {e}")
             temperature = None
+
+        try:
+            pressure = self.sense.get_pressure()
+        except Exception as e:
+            logging.error(f"Error reading pressure: {e}")
+            pressure = None
+
+        try:
+            humidity = self.sense.get_humidity()
+        except Exception as e:
+            logging.error(f"Error reading humidity: {e}")
             humidity = None
 
-        try:
-            lux = self.light.Lux()
-        except Exception as e:
-            logging.error(f"Error reading TSL2591 data: {e}")
-            lux = None
-
-        try:
-            uvs = self.uv.UVS()
-        except Exception as e:
-            logging.error(f"Error reading LTR390 data: {e}")
-            uvs = None
-
-        try:
-            gas = self.air_quality.measureRaw(temperature, humidity)
-        except Exception as e:
-            logging.error(f"Error reading SGP40 data: {e}")
-            gas = None
-
-        return pressure, temperature, humidity, lux, uvs, gas, reading_time
+        return pressure, temperature, humidity, reading_time
 
     def get_sensor_data_json(self):
-        pressure, temperature, humidity, lux, uvs, gas, reading_time = (
-            self.get_sensor_data()
-        )
+        pressure, temperature, humidity, reading_time = self.get_sensor_data()
         return {
             "pressure": pressure,
             "temperature": temperature,
             "humidity": humidity,
-            "lux": lux,
-            "uvs": uvs,
-            "gas": gas,
+            "lux": 0,
+            "uvs": 0,
+            "gas": 0,
             "timestamp": reading_time,
             "errors": {
                 "pressure": pressure is None,
                 "temperature": temperature is None,
                 "humidity": humidity is None,
-                "lux": lux is None,
-                "uvs": uvs is None,
-                "gas": gas is None,
+                "lux": False,
+                "uvs": False,
+                "gas": False,
             },
         }
 
--- rpi_scripts/sensors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .BME280 import BME280
-from .LTR390 import LTR390
-from .TSL2591 import TSL2591
-from .SGP40 import SGP40
